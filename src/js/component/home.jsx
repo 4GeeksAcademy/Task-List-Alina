b570f1a2783cfa5a07026bf47dc0e446603df368
Styoks <113370103+Styoks@users.noreply.github.com>
2024-05-27 19:40:27 +0000

    problema

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,34 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
-const Home = () => {
+const Home = () => {	
+	const arraySSD = JSON.parse(localStorage.getItem("ToDoList"))
+	
+	const [list, setList] = useState([...arraySSD])
+	
+	function enterPressed (event) {
+		if (event.keyCode === 13){
+			setList((prevArray)=> {
+				return [...prevArray, event.target.value]
+			})
+		}
+		localStorage.setItem("ToDoList", list?.length > 0 && JSON.stringify([list]));
+	}
+	
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1>TODO</h1>
+			<div>
+				<input type="text" onKeyDown={enterPressed} />
+				<ul>
+				{list?.length>0 && list.map((item, algo)=>{
+					console.log(algo);
+					return <li>{item}</li>
+				})}
+				</ul>
+			</div>
 		</div>
 	);
 };
